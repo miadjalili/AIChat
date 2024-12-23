a009d5d96c6631f116f0a0c8f6db5a3b581d9c09
miadjalili <jalilimiad@gmail.com>
2024-12-23 13:34:35 -0800

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Miad Jalili on 2024-12-23.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
